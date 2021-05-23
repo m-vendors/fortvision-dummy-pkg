1af301066419d6ec772b9f6f7b3ce20e07e80f25
M Vendors <m-vendors@protonmail.com>
2021-05-23 21:47:45 +0300

    Initial Commit

---
+++ registration.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE,
+    'fortvision_DummyPkg',
+    __DIR__
+);
